67ea819795115eb91409a3eb675028881e693dab
HERO-KUN <herokun.user@gmail.com>
2020-04-21 16:22:19 +0900

    added time refreshing code

--- code.js
+++ code.js
@@ -42,3 +42,68 @@ if(!preferences.useDarkMode){
 } else {
   darkmode.style.backgroundColor = 'rgba(0, 0, 0, ' + preferences.darkModeAmount + ')';
 }
+
+
+var digitEach = document.getElementById('time_digit_min_1').childNodes[1];
+var digitHeight = digitEach.clientHeight + parseInt(window.getComputedStyle(digitEach).marginBottom);
+var digits = [
+  document.getElementById('time_digit_hour_10'),
+  document.getElementById('time_digit_hour_1'),
+  document.getElementById('time_digit_min_10'),
+  document.getElementById('time_digit_min_1'),
+  document.getElementById('time_digit_sec_10'),
+  document.getElementById('time_digit_sec_1')
+];
+var pauseTime = false;
+var pastTimeData = [-1, -1, -1, -1, -1, -1];
+
+function refreshTime() {
+  if(pauseTime) return;
+  var currentTime = new Date();
+  var timeData = [
+    Math.floor(currentTime.getHours()/10),
+    currentTime.getHours()%10,
+    Math.floor(currentTime.getMinutes()/10),
+    currentTime.getMinutes()%10,
+    Math.floor(currentTime.getSeconds()/10),
+    currentTime.getSeconds()%10,
+  ];
+  for(var i = 0; i < digits.length; i++){
+    if(timeData[i] != pastTimeData[i]){
+      var player = digits[i].animate([
+        {top: 'calc(50% - ' + (pastTimeData[i] * digitHeight) + 'px)'},
+        {top: 'calc(50% - ' + (timeData[i] * digitHeight) + 'px)'}
+      ], {
+        duration: 275,
+        easing: "ease-out",
+        fill: "forwards"
+      });
+      digitDiv = digits[i].getElementsByTagName('div');
+      for(var j = 0; j < digitDiv.length; j++){
+        if(j == timeData[i]){
+          digitDiv[j].animate([
+            {opacity: window.getComputedStyle(digitDiv[j]).opacity},
+            {opacity: 1}
+          ], {
+            duration: 275,
+            fill: "forwards"
+          });
+        } else {
+          digitDiv[j].animate([
+            {opacity: window.getComputedStyle(digitDiv[j]).opacity},
+            {opacity: 0.3}
+          ], {
+            duration: 275,
+            fill: "forwards"
+          });
+        }
+      }
+    }
+  }
+  pastTimeData = timeData;
+  setTimeout(function() {
+    refreshTime();
+  }, 50);
+}
+
+refreshTime();
