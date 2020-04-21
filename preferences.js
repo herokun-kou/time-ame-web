cdcdbb015b830f69968af0095a91838a8eaed0dd
HERO-KUN <herokun.user@gmail.com>
2020-04-21 18:48:47 +0900

    added preference file

---
+++ preferences.js
@@ -0,0 +1,10 @@
+var preferences =
+{
+  bgVerticalGravity: "bottom",    /* valid value : top, bottom, center */
+  bgHorizontalGravity: "left",    /* valid value : left, right, center */
+  useBgBlur: false,
+  useDarkMode: true,
+  darkModeAmount: 0.4,            /* valid value : 0 ~ 1 float, 0 equals to useDarkMode: false */
+  mainText: "public class HeroKun extends Person { ... }",
+  subText: "마음만은 언제까지나 멍멍이 소년"
+}
